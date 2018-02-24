100caa38aea8ce017aa101d4c7e49ddf87af86e6
likuan <kk2n@qq.com>
2018-02-24 12:45:55 +0800

    up

--- src/page/entry.p1.js
+++ src/page/entry.p1.js
@@ -1,4 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<div>11111111</div>, document.getElementById('root'))
+ReactDOM.render(<div>11111111</div>, document.getElementById("root"));
